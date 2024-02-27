b14b29a103c6c9bfb59128be7a8c5839c514ab57
Juicyyyyyyy <declan.lowee@gmail.com>
2024-02-27 06:26:17 +0100

    Replace discord cdn for image with actual image from static

--- component_generator/settings.py
+++ component_generator/settings.py
@@ -123,6 +123,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Define the directories where Django will look for static files.
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
